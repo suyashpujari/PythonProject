7a9987e616329e5d4068dc2b0257a58fe0193811
Jain-nihhilkumar <nikhilkumar1514@gmail.com>
2021-09-06 08:12:34 -0700

    projrct inovice

---
+++ MINI-PROJECT/inovice.py
@@ -0,0 +1,112 @@
+
+from typing import ClassVar, DefaultDict, final
+
+
+print("ENTER YOUR NAME AND CONTACT NO:")
+A=str(input())
+print ("\nENTER THE PRODUCT ID OF PRODUCT U WANT TO BYE:")
+
+
+choice=int (input())
+
+
+    
+if choice==1 :
+    print("-----------------------WELCOME TO VARDHAN COMPUTERS----------------- \n \t\t1562,lane no.5 off parola road,Jalgoan,424001")
+    print("PHONE NO:(02565)\t\t\tGSTIN:27AEAPTO162J12ZI")
+    print("____________________________________________________________________")
+    print("M\S:",A)
+    print("\n")
+    print("PRODUCT _ID:1")
+    print("\n")
+    print("PRODUCT_NAME:MI XIAOMI NOTEBOOK 14 LAPTOP")
+    print("\n")
+    print ("SPECIFICATION:CORE i5/8GB/512GBSSD/WIN10/2GBMX250")
+    print("\n")
+    print("PRICE:47999")
+    print("\n")
+    print("---------------THANK-YOU FOR VISITING US VISIT AGAIN SOON-------------------")
+elif choice==2:
+    print("-----------------------WELCOME TO VARDHAN COMPUTERS----------------- \n \t\t1562,lane no.5 off parola road,Jalgoan,424001")
+    print("PHONE NO:(02565)\t\t\tGSTIN:27AEAPTO162J12ZI")
+    print("____________________________________________________________________")
+    print("M\S:",A)
+    print("PRODUCT _ID:2")
+    print("\n")
+    print("PRODUCT_NAME:ACER PREDATOR HELIOS 300 PH315_15_51_51V7")
+    print("\n")
+    print ("SPECIFICATION:CORE i7/16GB/1TB SSD/6GB 3060/WIN 10/15.6/")
+    print("\n")
+    print("PRICE:52999")
+    print("\n")
+    print("---------------THANK-YOU FOR VISITING US VISIT AGAIN SOON-------------------")
+
+elif choice==3:
+    print("-----------------------WELCOME TO VARDHAN COMPUTERS----------------- \n \t\t1562,lane no.5 off parola road,Jalgoan,424001")
+    print("PHONE NO:(02565)\t\t\tGSTIN:27AEAPTO162J12ZI")
+    print("____________________________________________________________________")
+    print("M\S:",A)
+    print("PRODUCT _ID:3")
+    print("\n")
+    print("PRODUCT_NAME:LENOVO IDEAPAD SLIM 5 PRO")
+    print("\n")
+    print ("SPECIFICATION:CORE i5/8GB/512 GBSSD/WIN 10/14.0")
+    print("\n")
+    print("PRICE:77990")
+    print("\n")
+    print("---------------THANK-YOU FOR VISITING US VISIT AGAIN SOON-------------------")
+
+
+elif choice==4:
+    print("-----------------------WELCOME TO VARDHAN COMPUTERS----------------- \n \t\t1562,lane no.5 off parola road,Jalgoan,424001")
+    print("PHONE NO:(02565)\t\t\tGSTIN:27AEAPTO162J12ZI")
+    print("____________________________________________________________________")
+    print("M\S:",A)
+    print("PRODUCT _ID:4")
+    print("\n")
+    print("PRODUCT_NAME:HP PAVILLION GAMING 15-ec010AX")
+    print("\n")
+    print("CORE i5/8GB/1TV HDD/R GB1650/15.6/WIN10")
+    print("\n")
+    print("PRICE:50990")
+    print("\n")
+    print("---------------THANK-YOU FOR VISITING US VISIT AGAIN SOON-------------------")
+
+elif choice==5:
+    print("-----------------------WELCOME TO VARDHAN COMPUTERS----------------- \n \t\t1562,lane no.5 off parola road,Jalgoan,424001")
+    print("PHONE NO:(02565)\t\t\tGSTIN:27AEAPTO162J12ZI")
+    print("____________________________________________________________________")
+    print("M\S:",A)
+    print("PRODUCT _ID:5")
+    print("\n")
+    print("PRODUCT_NAME:APPLE MACBOOK AIR MQD32HN/A ULTRABOOOK")
+    print("\n")
+    print ("SPECIFICATION:CORE i5 /8GB/128GB SSD/13.30/INTEGRATED HD GRAPHICS 6000/MAC OS")
+    print("PRICE:73490")
+    print("\n")
+    print("---------------THANK-YOU FOR VISITING US VISIT AGAIN SOON-------------------")
+
+elif choice==6:
+    print("-----------------------WELCOME TO VARDHAN COMPUTERS----------------- \n \t\t1562,lane no.5 off parola road,Jalgoan,424001")
+    print("PHONE NO:(02565)\t\t\tGSTIN:27AEAPTO162J12ZI")
+    print("____________________________________________________________________")
+    print("M\S:",A)
+    print("\nPRODUCT _ID:6")
+    print("\n")
+    print("PRODUCT_NAME:ASUS TUF F15")
+    print("\n")
+    print ("SPECIFICATION:core i5/8GB/1TB SSD/15.6/4GB1650Ti/WIN10")
+    print("\n")
+    print("PRICE:72000")
+    print("---------------THANK-YOU  FOR VISITING US  VISIT AGAIN SOON-------------------")
+
+else:
+    print("plz enter valid product id")
+
+
+final
+print("PLZ CONFORM YOUR PURCHASE  \n yes=1 \tno=2")
+if int(input()!=1):
+    print("PURCHASE SUCCESSFUL THANK YOU.........")
+else:
+    print("PURCHASE UNSUCESSFUL PLZ TRY AGIAN...")
